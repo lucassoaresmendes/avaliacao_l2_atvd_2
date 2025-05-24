14f54affd2d3a82861ccd259f82ebc47c964b453
lucassoaresmendes <lucassoaresmendes.info@gmail.com>
2025-05-24 17:57:45 -0300

    Primeiro commit

---
+++ src/main/java/com/l2/avaliacao_l2_atvd_2/AvaliacaoL2Atvd2Application.java
@@ -0,0 +1,13 @@
+package com.l2.avaliacao_l2_atvd_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoL2Atvd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoL2Atvd2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/l2/avaliacao_l2_atvd_2/AvaliacaoL2Atvd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l2.avaliacao_l2_atvd_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoL2Atvd2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
